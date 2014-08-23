c1cd0b0bcb37ce1b88a4cf111f420f51e46977ff
Maarten Paauw <maartenpaauw@gmail.com>
2014-08-23 03:36:27 +0200

    Overwrite Twitter button color

--- js/splitwitter-content.js
+++ js/splitwitter-content.js
@@ -1,5 +1,7 @@
 function injectCode() {
     $(".js-tweet-btn").css({ backgroundColor : '#942686', background : 'rgba(148, 38, 134, 1)' });
+    $(".tweet-btn").css({ backgroundColor : '#942686', background : 'rgba(148, 38, 134, 1)' });
+    $(".dm-new-button").css({ backgroundColor : '#942686', background : 'rgba(148, 38, 134, 1)', border : 'none' });
 }
 
 $(document).ready(function() {
