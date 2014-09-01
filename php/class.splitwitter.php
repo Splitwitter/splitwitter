b08809b6e783a0e552e93a3f3a774735eca4d537
Maarten Paauw <maartenpaauw@gmail.com>
2014-09-01 14:43:19 +0200

    Backup an old file.

--- php/class.splitwitter.php
+++ php/class.splitwitter.php
@@ -13,49 +13,69 @@
  */
 
 class splitwitter {
-
+    
     private $lenght = 140;
-
+    private $hashtag = "#splitwitter";
+    
     private $tweet;
+    private $promo;
     private $tweetArray;
-
-    public function __construct($tweet = null) {
+    
+    public function __construct($tweet = null, $promo = false) {
         if(isset($tweet) && $tweet != null) {
             $this->tweet = $tweet;
         }
+        
+        if(isset($promo) && $promo != null) {
+            $this->promo = $promo;
+        }
     }
-
+    
     public function tweet() {
         // Max Tweet lenght
         $length = $this->lenght;
-
+        
         // Add space for the numbering
         $length = $length - 8;
-
-        // Split up the tweet into an array
+        
+        // When promo is true add space for the promo hashtag
+        if($this->promo == true) {
+            $length = $length - strlen($this->hashtag);
+        }
+        
+        // Split up the Tweet into an array
         $valueArray = explode("\n", wordwrap($this->tweet, $length, "\n"));
-
+        
         // The amount of tweets
         $count = count($valueArray);
-
+        
         // The keys for the array
         $keyArray = range(1, $count);
-
+        
         // Combines the key- and value array
         $tweetArray = array_combine($keyArray, $valueArray);
-
+        
         // Add the Tweet numbering
         foreach($tweetArray as $key => &$value) {
-            $value = $value . " [" . $key . "/" . $count . "]";
+            
+            // Id promo is true add the promo hashtag to the last Tweet with the numbering.
+            if($key == $count && $this->promo == true) {
+                $value = $value . " " . $this->hashtag . " [" . $key . "/" . $count . "]";
+            }
+            
+            // Add the numbering to the Tweet.
+            else {
+                $value = $value . " [" . $key . "/" . $count . "]";
+            }
         }
-
+        
         $this->tweetArray = $tweetArray;
-        return $tweetArray;
+        return $tweetArray; 
     }
 }
 
 $tweet = "Maecenas pretium dolor quis lectus dictum laoreet. Vestibulum imperdiet magna in nisi luctus iaculis. Sed a magna vehicula, pellentesque ipsum in, venenatis magna. Duis diam diam, euismod quis pharetra tristique, interdum ac sapien. Pellentesque semper risus enim, non dignissim ante dapibus vel. Sed cursus, magna sit amet iaculis faucibus, purus justo fringilla lacus, at elementum leo purus aliquet velit. Nulla facilisi. Sed egestas quis lacus vel varius. Etiam vitae eros turpis. Maecenas augue lectus, fermentum quis egestas eu, dictum iaculis tellus. Praesent molestie ligula ipsum, sed elementum leo molestie ac.";
 
-$splitwitter = new splitwitter($tweet);
+$splitwitter = new splitwitter($tweet, true);
 echo "<pre>";
 print_r($splitwitter->tweet());
